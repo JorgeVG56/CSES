a83cfd00e6d5fa82be7a0c32c9a57c34fecbf5f2
JorgeVG56 <jorge.valdez743ciop6@gmail.com>
2025-10-30 09:45:33 -0700

    new file:   graph-algorithms/giant-pizza/2.in new file:   graph-algorithms/giant-pizza/A modified:   graph-algorithms/giant-pizza/A.cpp

--- graph-algorithms/giant-pizza/A.cpp
+++ graph-algorithms/giant-pizza/A.cpp
@@ -3,8 +3,8 @@ using namespace std;
 using ll = long long;
 
 struct TarjanSolver{
-  vector<vector<int>> revAdj;
   vector<vector<int>> & adj;
+  vector<vector<int>> revAdj;
   vector<int> post, comp;
   vector<bool> visited;
   int timer = 0;
@@ -34,7 +34,7 @@ struct TarjanSolver{
   void findComp(int u){
     visited[u] = true;
     comp[u] = id;
-    for(auto v : revAdj[u]) if(!visited[v]) findComp(v);
+    for(auto v : adj[u]) if(!visited[v]) findComp(v);
   }
 
   int compNum(){ return id; }
@@ -46,8 +46,8 @@ struct Clause{
   bool neg1, neg2;
 };
 
-vector<int> solveSat(vector<Clause> & clauses, int n){
-  vector<vector<int>> adj(2 * n);
+vector<int> solveSat(vector<Clause> & clauses, int nVar){
+  vector<vector<int>> adj(2 * nVar);
 
   for(auto & c : clauses){
     adj[2 * c.var1 + !c.neg1].push_back(2 * c.var2 + c.neg2);
@@ -56,13 +56,13 @@ vector<int> solveSat(vector<Clause> & clauses, int n){
 
   TarjanSolver ts(adj);
   vector<vector<int>> comps(ts.compNum());
-  for(int u = 0; u < 2 * n; u += 2){
+  for(int u = 0; u < 2 * nVar; u += 2){
     if(ts.getComp(u) == ts.getComp(u + 1)) return {};
     comps[ts.getComp(u)].push_back(u);
     comps[ts.getComp(u + 1)].push_back(u + 1);
   }
 
-  vector<int> val(2 * n,  -1);
+  vector<int> val(2 * nVar,  -1);
   for(auto & comp : comps){
     int setTo = 1;
     for(int u : comp){
@@ -76,13 +76,37 @@ vector<int> solveSat(vector<Clause> & clauses, int n){
     for(int u : comp) val[u] = setTo;
   }
 
-  vector<int> actualVal(n);
-	for (int i = 0; i < n; i++) actualVal[i] = val[2 * i];
+  vector<int> actualVal(nVar);
+	for (int i = 0; i < nVar; i++) actualVal[i] = val[2 * i];
 	return actualVal;
 }
 
 signed main(){
   cin.tie(0)->sync_with_stdio(0);
+
+  int n, m; cin >> n >> m;
+
+  vector<Clause> clauses(n);
+  for(int i = 0; i < n; i++){
+    char s1; cin >> s1; 
+    int x1; cin >> x1;
+    char s2; cin >> s2; 
+    int x2; cin >> x2;
+
+    clauses[i] = {x1 - 1, x2 - 1, s1 == '-', s2 == '-'};
+  }
+
+  vector<int> solution = solveSat(clauses, m);
+
+  if(solution.empty()){
+    cout << "IMPOSSIBLE";
+  } else{
+    for(int i = 0; i < m; i++){
+      cout << "-+"[solution[i]] << ' ';
+    }
+  }
+
+  cout << '\n';
 
   return 0;
 }
